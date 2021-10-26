5f58afa4edd321ee2a3b157990f5bec2748900ce
Michael Polidori <contact@michaelpolidori.com>
2021-10-26 15:46:23 -0400

    Check CKAN version before using JSON validator

--- ckanext/translate/logic/schema.py
+++ ckanext/translate/logic/schema.py
@@ -3,9 +3,14 @@ import string
 
 def translate():
     not_empty = tk.get_validator("not_empty")
-    json_object = tk.get_validator("json_object")
+    input_validators = [not_empty]
+
+    if tk.check_ckan_version(min_version='2.8.5'):
+        json_object = tk.get_validator("json_object")
+        input_validators = [not_empty, json_object]
+
     return {
-        "input": [not_empty, json_object],
+        "input": input_validators,
         "from": [not_empty],
         "to": [not_empty],
     }
